49c695705cd3a5f527720f8b71e84f011dc5392e
apeing <xiaolong1988_good@yeah.net>
2017-09-01 17:25:57 +0800

    create

---
+++ app.js
@@ -0,0 +1,34 @@
+﻿const express = require('express');
+const app = express();
+
+app.use(require('serve-favicon')(__dirname + '/favicon.ico'));
+app.use(require('cookie-parser')());
+app.use(require('compression')());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const config = {
+    "db" : {
+    "host": "localhost:27017",
+    "name": "wechat",
+    "username": "wechat",
+    "password": "wechat",
+    "options": {
+        "server": {
+            "poolSize": 10,
+            "socketOptions": {
+                "keepAlive": 1
+            }
+        }
+    }
+}};
+require('./app/lib/mongoose.js')(config.db);
+
+var path = require('path');
+app.use(express.static(path.join(__dirname, './public')));
+
+app.use('/', require('./app/router.js'));
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,7 @@
+﻿var app = require('./app');
+var meta = require('./package.json');
+
+var port = '80';
+var server = app.listen(port,function(){
+  console.log('【%s】正在监听端口: %s', meta.name, server.address().port);
+})
